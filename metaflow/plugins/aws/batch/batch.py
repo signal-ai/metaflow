1c59c6590f36b969d266a4d232994d3f216e26d2
Thibaut Sacreste <thibaut.sacreste@gmail.com>
2025-12-11 17:47:33 +0000

    Fix incorrect quote escaping in the S3 command download script. Trying again

--- metaflow/plugins/aws/batch/batch.py
+++ metaflow/plugins/aws/batch/batch.py
@@ -122,14 +122,15 @@ class Batch(object):
         # Get the command that was created
         # Upload the command to S3 during deployment
         try:
-            command_bytes = cmd_str.encode("utf-8")
+            unescaped_cmd_str = cmd_str.replace('\\"', '"')
+            command_bytes = unescaped_cmd_str.encode("utf-8")
             result_paths = self.flow_datastore.save_data([command_bytes], len_hint=1)
             s3_path, _key = result_paths[0]
 
             bucket, s3_object = parse_s3_full_path(s3_path)
             download_script = "{python} -c '{script}'".format(
                 python=self.environment._python(),
-                script='import boto3, os; ep=os.getenv("METAFLOW_S3_ENDPOINT_URL"); boto3.client("s3", **({"endpoint_url":ep} if ep else {})).download_file("%s", "%s", "/tmp/step_command.sh")'
+                script='import boto3, os; ep=os.getenv(\\"METAFLOW_S3_ENDPOINT_URL\\"); boto3.client(\\"s3\\", **({\\"endpoint_url\\":ep} if ep else {})).download_file(\\"%s\\", \\"%s\\", \\"/tmp/step_command.sh\\")'
                 % (bucket, s3_object),
             )
             download_cmd = (
